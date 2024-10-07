e96360a87f7b62f7f5b85ef7bdf632919ee16b14
CodyKoInABox <codykoinabox@gmail.com>
2024-10-06 22:22:59 -0300

    api should be working

--- api/api.js
+++ api/api.js
@@ -1,3 +1,7 @@
+let express = require('express')
+let app = express();
+const port = 8080;
+
 const mysql = require('mysql');
 
 require('dotenv').config();
@@ -10,8 +14,62 @@ let connection = mysql.createConnection({
     port: process.env.DB_PORT
   });
 
-connection.connect();
+
+app.get('signUp/:username/:password', (req, res) => {
+
+    let username = req.params.username
+    let password = req.params.password
+
+    connection.connect();
+
+    connection.query(`INSERT INTO users (username, password) VALUES ("${username}", "${password}")`, function (error, results, field){
+
+        console.log(results)
+
+        if(error){
+
+            res.send(400)
+        }
+        else{
+
+            res.send(200)
+        }
+    })
+
+})
 
 
+app.get('login/:username/:password', (req, res) => {
 
-connection.end();
+    let username = req.params.username
+    let password = req.params.password
+
+    connection.connect();
+
+    connection.query(`SELECT * FROM users WHERE username = ${username}`, function (error, results, field){
+        
+        console.log(results)
+
+        if(results[0].password == password){
+
+            // login successful
+
+            res.send(200)
+
+        }
+        else{
+
+            // login not successful
+
+            res.send(400)
+        }
+    });
+
+})
+
+app.listen(port, () => {
+    
+    console.log('API is live!');
+
+    
+})
---
+++ api/db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `users`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL
+);
