c81d7191eeb02a0430a2fee12bb4b656dfb852a6
CodyKoInABox <codykoinabox@gmail.com>
2024-10-06 21:09:46 -0300

    button to show/hide password

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 
 
 
-
 document.addEventListener('DOMContentLoaded', function() {
-    
     const loginForm = document.getElementById('loginForm');
-    
+    const passwordInput = document.getElementById('password');
+    const togglePassword = document.getElementById('togglePassword');
+
+    // toggle password
+    togglePassword.addEventListener('click', function() {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+
+        
+        if (type === 'text') {
+            togglePassword.src = './assets/hide.png'; 
+        } else {
+            togglePassword.src = './assets/show.png';
+        }
+    });
+
+
     loginForm.addEventListener('submit', function(event) {
 
         event.preventDefault();
         
         const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
+        const password = passwordInput.value;
         
         console.log('Username:', username);
         console.log('Password:', password);
-        
-        
     });
 });
